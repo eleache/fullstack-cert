00a84cd998a47e90448356c9a5ba13060b83c888
Crybaby <exampleinternet@outlook.com>
2021-09-23 03:43:17 -0500

    'format'

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
                     filename: 'bundle.js'
           }
 
-          plugins: [
-                    new 
-          ]
+          //plugins: [
+          //          new 
+          //]
 };
