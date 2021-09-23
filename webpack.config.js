cb93d241a41e4b4aea866eb5f111d2175ab98d31
Crybaby <exampleinternet@outlook.com>
2021-09-23 03:11:49 -0500

    'union model to schema'

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+          
+module.exports = {
+          entry: './frontend/app.js',
+          output: {
+                    path: path.join(__dirname, 'backend/public'),
+                    filename: 'bundle.js'
+          }
+
+          plugins: [
+                    new 
+          ]
+};
